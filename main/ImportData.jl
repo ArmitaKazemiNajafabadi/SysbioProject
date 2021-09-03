863cceac775cbab86fd5b1f4e48270b29b3e6d47
aeirya <aeirya@gmail.com>
2021-09-03 11:39:43 +0430

    Create notebook

--- main/ImportData.jl
+++ main/ImportData.jl
@@ -1,7 +1,12 @@
 # importing packages
 using Pkg
 Pkg.add("SBML")
+Pkg.add("JuMP")
+Pkg.add("GLPK")
 
 using SBML
-model = readSBML("resources/e-coli-core.xml")
+# model = readSBML("resources/e-coli-core.xml")
 
+using JuMP, GLPK
+
+sparse()
